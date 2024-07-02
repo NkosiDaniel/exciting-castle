3a91a3f6b2db71a9c2a66d553f6e3f38b93b0ffb
Ayinde Abrams <xxxenouniverse@gmail.com>
2024-07-01 20:42:13 -0400

    Fonts and Tilemap Files

---
+++ Assets/Tilemaps/DungeonIsometricTileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="DungeonIsometricTileset" tilewidth="32" tileheight="32" margin="16" tilecount="135" columns="9">
+ <image source="../../../Downloads/DungeonIsometricTileset.png" width="304" height="496"/>
+</tileset>
